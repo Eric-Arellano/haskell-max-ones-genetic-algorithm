d1394b04d6c51223e1beb08b9ecbbc4eb168ae1d
Eric Arellano <ericarellano@me.com>
2019-01-15 22:55:39 -0700

    Update main to pull values from config

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,10 @@ module Main where
 import qualified Lib
 import qualified Config
 
+import qualified System.Random as Random
+
 main :: IO ()
 main = do
     config <- Config.readConfig "resources/config.cfg"
-    putStrLn $ show config
+    let pop = Lib.mkPopulation (Random.mkStdGen $ Config.seed config) (Config.numIndividuals config) (Config.numGenes config)
+    putStrLn $ show $ map Lib.individualToBitStr pop
