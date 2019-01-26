50fe770b11ef5cffd5e98b366847cadd6b330ed3
Eric Arellano <ericarellano@me.com>
2019-01-26 16:01:10 -0700

    Implement first quickcheck for project
    I'm not sure how to get other properties to be tested effectively. For now, this is a good first pass and I can flesh this out over time.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,14 @@
-import Test.QuickCheck
+{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import System.Random (mkStdGen)
+import Test.QuickCheck (quickCheck)
+
+import qualified Lib
 
 main :: IO ()
 main = do
-    quickCheck prop_reverse
+    quickCheck $ prop_sameLength (Lib.mutate (mkStdGen 1) 0.4)
 
-prop_reverse :: [Int] -> Bool
-prop_reverse xs = reverse (reverse xs) == xs
+prop_sameLength :: forall a. ([a] -> [a]) -> [a] -> Bool
+prop_sameLength f xs = length (f xs) == length xs 
